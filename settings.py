f8cabae76ca1c0d5eed261ff7d77401be680b87b
m255178 <115946602+m255178@users.noreply.github.com>
2022-10-25 09:02:17 -0400

    Finished Video Game

--- settings.py
+++ settings.py
@@ -6,3 +6,9 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (230,230,230)
+        self.ship_speed = 1.5
+        self.bullet_speed = 1.0
+        self.bullet_width = 0.00001
+        self.bullet_height = 10
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 3
