88b078f9875a904ba2463d4e851292d1369d1b0c
m255178 <115946602+m255178@users.noreply.github.com>
2022-10-25 09:09:17 -0400

    Finished Video Game

--- settings.py
+++ settings.py
@@ -4,11 +4,11 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         self.screen_width = 1200
-        self.screen_height = 600
+        self.screen_height = 400
         self.bg_color = (230,230,230)
         self.ship_speed = 1.5
-        self.bullet_speed = 1.0
-        self.bullet_width = 0.00001
-        self.bullet_height = 10
+        self.bullet_speed = 10.0
+        self.bullet_width = 20
+        self.bullet_height = 3
         self.bullet_color = (60,60,60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 30
